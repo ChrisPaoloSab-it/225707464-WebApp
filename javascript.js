9f1fcf11e6744364ce262265e42a58afe06bb1bc
ChrisPaoloSab-it <cpaocastillos@gmail.com>
2024-01-29 17:03:18 +0800

    copied code from activities, research how to link css and js into html5 doc

---
+++ javascript.js
@@ -0,0 +1,11 @@
+// alert when page loads
+window.onload = function () {
+    alert("WELCOME!!!!");
+}
+
+//handling a button click?
+function displayMessage() {
+    var message = "Button clicked!";
+    console.log(message);
+    alert(message)
+}
